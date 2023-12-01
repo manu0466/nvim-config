ace6ca6c4074e2838c2fe15613fdbce1c32c168f
Manuel <manuel.turetta94@gmail.com>
2023-12-01 12:30:19 +0100

    feat: add diffview plugin

---
+++ lua/manu/plugins/diffview.lua
@@ -0,0 +1,11 @@
+-- diffview.nvim: to review the file changes.
+return {
+    'sindrets/diffview.nvim',
+    lazy = false,
+    keys = {
+        { "<leader>gd", "<cmd>DiffviewOpen<cr>", mode = "n", desc = "Open diffview" },
+        { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", mode = "n", desc = "Open diffview current file history" },
+        { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", mode = "n", desc = "Open diffview branch history" },
+        { "<leader>gc", "<cmd>DiffviewClose<cr>", mode = "n", desc = "Close diffview" },
+    }
+}
