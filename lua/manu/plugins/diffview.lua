82bf06e252448d46f25f7a08af523389b89df299
Manuel <manuel.turetta94@gmail.com>
2024-01-16 21:54:25 +0100

    feat: lazy load diffview

--- lua/manu/plugins/diffview.lua
+++ lua/manu/plugins/diffview.lua
@@ -1,11 +1,12 @@
 -- diffview.nvim: to review the file changes.
 return {
     'sindrets/diffview.nvim',
-    lazy = false,
+    lazy = true,
+    cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
     keys = {
-        { "<leader>gd", "<cmd>DiffviewOpen<cr>", mode = "n", desc = "Open diffview" },
+        { "<leader>gd", "<cmd>DiffviewOpen<cr>",          mode = "n", desc = "Open diffview" },
         { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", mode = "n", desc = "Open diffview current file history" },
-        { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", mode = "n", desc = "Open diffview branch history" },
-        { "<leader>gc", "<cmd>DiffviewClose<cr>", mode = "n", desc = "Close diffview" },
+        { "<leader>gH", "<cmd>DiffviewFileHistory<cr>",   mode = "n", desc = "Open diffview branch history" },
+        { "<leader>gc", "<cmd>DiffviewClose<cr>",         mode = "n", desc = "Close diffview" },
     }
 }
