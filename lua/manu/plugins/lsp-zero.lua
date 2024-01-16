cc68c599efe8db7cfe12b6ffd37de2aebba27439
Manuel <manuel.turetta94@gmail.com>
2024-01-16 19:41:37 +0100

    feat: migrate to lsp-zero v3.0

--- lua/manu/plugins/lsp-zero.lua
+++ lua/manu/plugins/lsp-zero.lua
@@ -1,6 +1,6 @@
 return {
     "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
+    branch = "v3.x",
     lazy = false,
     dependencies = {
         -- LSP Support
@@ -22,37 +22,25 @@ return {
 
     },
     config = function()
-        local lsp = require("lsp-zero")
+        local lsp_zero = require("lsp-zero")
         local formatting_augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-        lsp.preset("minimal")
-        lsp.ensure_installed({
-            "tsserver",
-            "rust_analyzer",
-            "gopls",
-        })
-
         -- Disable highlight since we use treesitter
-        lsp.set_server_config({
+        lsp_zero.set_server_config({
             on_init = function(client)
                 client.server_capabilities.semanticTokensProvider = nil
             end,
         })
 
-        -- Fix Undefined global 'vim'
-        lsp.nvim_workspace()
-
-        lsp.set_preferences({
-            suggest_lsp_servers = false,
-            sign_icons = {
-                error = "E",
-                warn = "W",
-                hint = "H",
-                info = "I",
-            },
+        lsp_zero.set_sign_icons({
+            error = "E",
+            warn = "W",
+            hint = "H",
+            info = "I",
         })
 
-        lsp.on_attach(function(client, bufnr)
+
+        lsp_zero.on_attach(function(client, bufnr)
             local opts = function(desc)
                 return { buffer = bufnr, remap = false, desc = desc }
             end
@@ -118,126 +106,135 @@ return {
             end, opts("LSP: Format"))
         end)
 
+        -- Automatic language server config
         local lspconfig = require("lspconfig")
-
-        -- Configure lua language server for neovim
-        lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-
-        -- tsserver
-        lspconfig.tsserver.setup({
-            on_attach = function(_, bufnr)
-                -- For tsserver we don't want the format on save.
-                vim.api.nvim_clear_autocmds({ group = formatting_augroup, buffer = bufnr })
-            end
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            ensure_installed = { 'tsserver', 'rust_analyzer', 'gopls' },
+            handlers = {
+                lsp_zero.default_setup,
+                lua_ls = function()
+                    local lua_opts = lsp_zero.nvim_lua_ls()
+                    lspconfig.lua_ls.setup(lua_opts)
+                end,
+                tsserver = function()
+                    lspconfig.tsserver.setup({
+                        on_attach = function(_, bufnr)
+                            -- For tsserver we don't want the format on save.
+                            vim.api.nvim_clear_autocmds({ group = formatting_augroup, buffer = bufnr })
+                        end
+                    })
+                end,
+                eslint = function()
+                    lspconfig.eslint.setup({
+                        settings = {
+                            codeAction = {
+                                disableRuleComment = {
+                                    enable = true,
+                                    location = "separateLine",
+                                },
+                                showDocumentation = {
+                                    enable = true,
+                                },
+                            },
+                            codeActionOnSave = {
+                                enable = true,
+                                mode = "all",
+                            },
+                            experimental = {
+                                useFlatConfig = false,
+                            },
+                            format = true,
+                            nodePath = "",
+                            onIgnoredFiles = "off",
+                            packageManager = "yarn",
+                            problems = {
+                                shortenToSingleLine = false,
+                            },
+                            quiet = false,
+                            rulesCustomizations = {},
+                            run = "onType",
+                            useESLintClass = false,
+                            validate = "on",
+                            workingDirectory = {
+                                mode = "location",
+                            },
+                        },
+                        on_attach = function(_, bufnr)
+                            vim.api.nvim_clear_autocmds({ group = formatting_augroup, buffer = bufnr })
+                            vim.api.nvim_create_autocmd("BufWritePre", {
+                                group = formatting_augroup,
+                                buffer = bufnr,
+                                callback = function()
+                                    vim.cmd("EslintFixAll")
+                                end,
+                            })
+                        end,
+                    })
+                end,
+                rust_analyzer = function()
+                    lspconfig.rust_analyzer.setup({
+                        -- Server-specific settings. See `:help lspconfig-setup`
+                        settings = {
+                            ['rust-analyzer'] = {
+                                cargo = {
+                                    -- Run the anlyzer with all the features
+                                    -- features = { "all" },
+                                },
+                                check = {
+                                    -- Use carco clippy to check the code instead of
+                                    -- cargo check
+                                    overrideCommand = {
+                                        "cargo",
+                                        "clippy",
+                                        "--all-targets",
+                                        "--all-features",
+                                        "--message-format=json",
+                                    }
+                                }
+                            },
+                        },
+                    })
+                end,
+                gopls = function()
+                    local util = require "lspconfig/util"
+                    lspconfig.gopls.setup({
+                        cmd = { "gopls", "serve" },
+                        filetypes = { "go", "gomod" },
+                        root_dir = util.root_pattern("go.work", "go.mod"),
+                        settings = {
+                            gopls = {
+                                analyses = {
+                                    unusedparams = true,
+                                },
+                                staticcheck = true,
+                            },
+                        },
+                        on_attach = function(_, bufnr)
+                            vim.api.nvim_clear_autocmds({ group = formatting_augroup, buffer = bufnr })
+                            vim.api.nvim_create_autocmd('BufWritePre', {
+                                group = formatting_augroup,
+                                buffer = bufnr,
+                                pattern = '*.go',
+                                callback = function()
+                                    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+                                end
+                            })
+                        end
+                    })
+                end,
+            }
         })
 
-        -- eslint
-        lspconfig.eslint.setup({
-            settings = {
-                codeAction = {
-                    disableRuleComment = {
-                        enable = true,
-                        location = "separateLine",
-                    },
-                    showDocumentation = {
-                        enable = true,
-                    },
-                },
-                codeActionOnSave = {
-                    enable = true,
-                    mode = "all",
-                },
-                experimental = {
-                    useFlatConfig = false,
-                },
-                format = true,
-                nodePath = "",
-                onIgnoredFiles = "off",
-                packageManager = "yarn",
-                problems = {
-                    shortenToSingleLine = false,
-                },
-                quiet = false,
-                rulesCustomizations = {},
-                run = "onType",
-                useESLintClass = false,
-                validate = "on",
-                workingDirectory = {
-                    mode = "location",
-                },
-            },
-            on_attach = function(_, bufnr)
-                vim.api.nvim_clear_autocmds({ group = formatting_augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = formatting_augroup,
-                    buffer = bufnr,
-                    callback = function()
-                        vim.cmd("EslintFixAll")
-                    end,
-                })
-            end,
-        })
-
-        -- rust analyzer
-        lspconfig.rust_analyzer.setup({
-            -- Server-specific settings. See `:help lspconfig-setup`
-            settings = {
-                ['rust-analyzer'] = {
-                    cargo = {
-                        -- Run the anlyzer with all the features
-                        features = { "all" },
-                    },
-                    check = {
-                        -- Use carco clippy to check the code instead of
-                        -- cargo check
-                        overrideCommand = {
-                            "cargo",
-                            "clippy",
-                            "--all-targets",
-                            "--all-features",
-                            "--message-format=json",
-                        }
-                    }
-                },
-            },
-        })
-
-        -- gopls
-        local util = require "lspconfig/util"
-        lspconfig.gopls.setup({
-            cmd = { "gopls", "serve" },
-            filetypes = { "go", "gomod" },
-            root_dir = util.root_pattern("go.work", "go.mod"),
-            settings = {
-                gopls = {
-                    analyses = {
-                        unusedparams = true,
-                    },
-                    staticcheck = true,
-                },
-            },
-            on_attach = function(_, bufnr)
-                vim.api.nvim_clear_autocmds({ group = formatting_augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd('BufWritePre', {
-                    group = formatting_augroup,
-                    buffer = bufnr,
-                    pattern = '*.go',
-                    callback = function()
-                        vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
-                    end
-                })
-            end
-        })
-
-        lsp.setup()
-
+        -- cmp config
         local cmp = require("cmp")
+        local cmp_format = lsp_zero.cmp_format()
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
         local cmp_mappings = {
             ["<Up>"] = cmp.mapping.select_prev_item(cmp_select),
             ["<Down>"] = cmp.mapping.select_next_item(cmp_select),
             ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            ["<C-e>"] = cmp.mapping.abort(),
+            ["<C-x>"] = cmp.mapping.abort(),
             ["<C-Space>"] = cmp.mapping.complete(),
         }
 
@@ -245,6 +242,7 @@ return {
         require("luasnip.loaders.from_vscode").lazy_load()
 
         cmp.setup({
+            formatting = cmp_format,
             sources = {
                 { name = "nvim_lsp" },
                 { name = "buffer" },
