a858a5c2c48640204fb8a0a7f26b96bfa9bce916
Manuel <manuel.turetta94@gmail.com>
2024-01-16 19:57:00 +0100

    feat: use LuaSnip v2.*

--- lua/manu/plugins/lsp-zero.lua
+++ lua/manu/plugins/lsp-zero.lua
@@ -17,6 +17,8 @@ return {
         {
             -- snippet plugin
             "L3MON4D3/LuaSnip",
+            version = "v2.*",
+            build = "make install_jsregexp",
             dependencies = "rafamadriz/friendly-snippets",
         },
 
