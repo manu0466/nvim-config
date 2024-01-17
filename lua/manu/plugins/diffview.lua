eec0046624d7373125cfae35bef69ba0052f6b66
Manuel <manuel.turetta94@gmail.com>
2024-01-17 10:31:02 +0100

    feat: use the / char instead of - in diffview

--- lua/manu/plugins/diffview.lua
+++ lua/manu/plugins/diffview.lua
@@ -3,6 +3,9 @@ return {
     'sindrets/diffview.nvim',
     lazy = true,
     cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+    init = function()
+        vim.opt.fillchars:append { diff = "╱" }
+    end,
     keys = {
         { "<leader>gd", "<cmd>DiffviewOpen<cr>",          mode = "n", desc = "Open diffview" },
         { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", mode = "n", desc = "Open diffview current file history" },
