64d61f7f96bf9480cf4dda1421d3451e9358596e
Manuel <manuel.turetta94@gmail.com>
2024-01-19 08:21:20 +0100

    feat: enable spell check

--- lua/manu/set.lua
+++ lua/manu/set.lua
@@ -29,3 +29,7 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- Spellcheck
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
