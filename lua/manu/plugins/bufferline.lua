b17332cb24032c85e1c1ac4fed65e80921c89fd1
Manuel <git.valuables901@passfwd.com>
2024-07-29 12:23:16 +0200

    feat(buffeline): use main branch

--- lua/manu/plugins/bufferline.lua
+++ lua/manu/plugins/bufferline.lua
@@ -1,6 +1,7 @@
 return {
     'akinsho/bufferline.nvim',
-    version = "*",
+    -- version = "*",
+    branch = 'main',
     dependencies = 'nvim-tree/nvim-web-devicons',
     lazy = false,
     config = function()
