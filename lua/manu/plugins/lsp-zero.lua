867a3e5fc7e65cbf58fe352e40b77e761019485f
Manuel <manuel.turetta@pm.me>
2024-09-18 16:40:07 +0200

    fix(lsp): golang import format

--- lua/manu/plugins/lsp-zero.lua
+++ lua/manu/plugins/lsp-zero.lua
@@ -280,7 +280,7 @@ return {
                         local util = require "lspconfig/util"
                         lspconfig.gopls.setup({
                             cmd = { "gopls", "serve" },
-                            filetypes = { "go", "gomod" },
+                            filetypes = { "go", "gomod", "gowork", "gotmpl" },
                             root_dir = util.root_pattern("go.work", "go.mod"),
                             settings = {
                                 gopls = {
@@ -288,13 +288,26 @@ return {
                                         unusedparams = true,
                                     },
                                     staticcheck = true,
+                                    gofumpt = true,
                                 },
                             },
-                            on_attach = function(client, bufnr)
+                            on_attach = function(_, _)
                                 vim.api.nvim_create_autocmd('BufWritePre', {
-                                    buffer = bufnr,
+                                    pattern = '*.go',
                                     callback = function()
-                                        vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+                                        local params = vim.lsp.util.make_range_params()
+                                        params.context = { only = { "source.organizeImports" } }
+                                        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                                        for cid, res in pairs(result or {}) do
+                                            for _, r in pairs(res.result or {}) do
+                                                if r.edit then
+                                                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or
+                                                        "utf-16"
+                                                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                                                end
+                                            end
+                                        end
+                                        vim.lsp.buf.format({ async = false })
                                     end
                                 })
                             end
