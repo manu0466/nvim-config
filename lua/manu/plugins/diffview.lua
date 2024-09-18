8c85a64e005be663075a9832ec206d27f0b1bac4
Manuel <manuel.turetta@pm.me>
2024-09-18 16:43:02 +0200

    feat(diffview): add keybinding to diffview from HEAD

--- lua/manu/plugins/diffview.lua
+++ lua/manu/plugins/diffview.lua
@@ -7,9 +7,10 @@ return {
         vim.opt.fillchars:append { diff = "╱" }
     end,
     keys = {
-        { "<leader>gd", "<cmd>DiffviewOpen<cr>",          mode = "n", desc = "Open diffview" },
-        { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", mode = "n", desc = "Open diffview current file history" },
-        { "<leader>gH", "<cmd>DiffviewFileHistory<cr>",   mode = "n", desc = "Open diffview branch history" },
-        { "<leader>gc", "<cmd>DiffviewClose<cr>",         mode = "n", desc = "Close diffview" },
+        { "<leader>gd", "<cmd>DiffviewOpen<cr>",                     mode = "n", desc = "Open diffview" },
+        { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>",            mode = "n", desc = "Open diffview current file history" },
+        { "<leader>gH", "<cmd>DiffviewFileHistory<cr>",              mode = "n", desc = "Open diffview branch history" },
+        { "<leader>gm", "<cmd>:DiffviewOpen origin/main...HEAD<cr>", mode = "n", desc = "diffview HEAD with origin/main" },
+        { "<leader>gc", "<cmd>DiffviewClose<cr>",                    mode = "n", desc = "Close diffview" },
     }
 }
