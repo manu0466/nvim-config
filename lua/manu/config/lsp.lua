04bb53e49ba8e30942a8c57cc228ede78aeef612
Manuel <manuel.turetta@pm.me>
2025-10-22 11:42:49 +0200

    feat: use conform to format buffers

--- lua/manu/config/lsp.lua
+++ lua/manu/config/lsp.lua
@@ -82,26 +82,5 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set("i", "<C-h>", function()
             vim.lsp.buf.signature_help()
         end, opts("LSP: Signature Help"))
-
-        vim.keymap.set("n", "<leader>ft", function()
-            if client.name == 'bashls' then
-                require("shfmt").formatting()
-            else
-                vim.lsp.buf.format()
-            end
-        end, opts("LSP: Format Text"))
-
-        -- Auto-format ("lint") on save.
-        -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-        if not client:supports_method('textDocument/willSaveWaitUntil')
-            and client:supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-                buffer = args.buf,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-                end,
-            })
-        end
     end,
 })
---
+++ lua/manu/plugins/conform.lua
@@ -0,0 +1,30 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                sql = { "sqlfmt" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+            },
+
+        })
+    end,
+    keys = {
+        {
+            -- Customize or remove this keymap to your liking
+            "<leader>ft",
+            function()
+                require("conform").format({ async = true })
+            end,
+            mode = "n",
+            desc = "Format buffer",
+        },
+    },
+}
