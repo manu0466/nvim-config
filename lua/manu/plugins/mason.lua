a0ed6c626b5ac7812042ee048f176c4626917cd2
Manuel <manuel.turetta@pm.me>
2025-10-31 14:14:59 +0100

    feat: add mason auto install

--- lua/manu/plugins/mason.lua
+++ lua/manu/plugins/mason.lua
@@ -1,5 +1,36 @@
 return {
-    'williamboman/mason.nvim',
+    'owallb/mason-auto-install.nvim',
+    dependencies = {
+        {
+            'williamboman/mason.nvim',
+            config = true,
+        }
+    },
+    opts = {
+        packages = {
+            -- LUA
+            "lua-language-server",
+            -- PHP
+            "phpactor",
+            "laravel-ls",
+            -- Python
+            "python-lsp-server",
+            -- Golang
+            "gopls",
+            -- Rust
+            "rust-analyzer",
+            -- Arduino
+            "arduino-language-server",
+            -- Solidity
+            "nomicfoundation-solidity-language-server",
+            -- Markdown
+            "marksman",
+            -- Yaml
+            "yaml-language-server",
+
+            "shfmt",
+            "sqlfmt",
+        }
+    },
     lazy = false,
-    config = true,
 }
