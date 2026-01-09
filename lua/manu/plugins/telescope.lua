b10fdbdd95137f30ad6ac58c2822a8bf2585e97c
Manuel <manuel.turetta@pm.me>
2026-01-09 09:50:33 +0100

    chore: bump telescope to v0.2.1

--- lua/manu/plugins/telescope.lua
+++ lua/manu/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.3",
+    tag = 'v0.2.1',
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-media-files.nvim",
