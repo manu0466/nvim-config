42fc7d6f779c7a81dc8105336ceb5ccb29f6c521
manu0466 <git.valuables901@passfwd.com>
2026-05-09 21:14:41 +0200

    fix: load treesitter on json and css files

--- lua/manu/config/treesitter.lua
+++ lua/manu/config/treesitter.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'lua', 'rust', 'php', 'tex', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+    pattern = { 'lua', 'rust', 'php', 'tex', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'json', 'css' },
     callback = function()
         -- syntax highlighting, provided by Neovim
         vim.treesitter.start()
